33d7280b6d8f111a61ffff409e1da62b4f8d4363
FreNior <124529660+FreNior@users.noreply.github.com>
2023-02-05 17:19:55 -0300

    Add PrimitiveScoreboard via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let scoreCountHome = 0
+let scoreCountGuest = 0
+
+let scorHom = document.getElementById("num-sco1");
+let scorGue = document.getElementById("num-sco2");
+
+function resetScoreHome() {
+     return scoreCountHome = 0
+     }
+
+function plusOneH() {
+    scoreCountHome += 1
+    scorHom.textContent = scoreCountHome
+    }
+
+function plusTwoH() {
+    scoreCountHome += 2
+    scorHom.textContent = scoreCountHome
+    }
+
+function plusThreeH() {
+    scoreCountHome += 3
+    scorHom.textContent = scoreCountHome
+    }
+
+function resetScoreGuest() {
+     scoreCountGuest = 0
+}
+function plusOneG() {
+    scoreCountGuest += 1
+    scorGue.textContent = scoreCountGuest
+    }
+
+function plusTwoG() {
+    scoreCountGuest += 2
+    scorGue.textContent = scoreCountGuest
+}
+
+function plusThreeG() {
+    scoreCountGuest += 3
+    scorGue.textContent = scoreCountGuest
+}
